2f73cfbbdeebfde2e8168e6768ab6e8602e76e21
czhizhpon <czhizhpon@est.ups.edu.ec>
2019-07-25 15:50:00 -0500

    Programación de los controladores CompanyController, PrivilegeController, VehCategoryController

---
+++ src/ec/edu/ups/controller/CompanyController.java
@@ -0,0 +1,36 @@
+/**
+ * @(#)CompanyController.java	0.0.1, 22-Jul-2019 
+ * 
+ * Universidad Politécnica Salesiana
+ * Carrera de Computación
+ * Cuenca - Ecuador	
+ */
+package ec.edu.ups.controller;
+
+/**
+ * @since 22-Jul-2019
+ * @version 0.0.1
+ * @author Sarmiento Bryan, Serpa Roberto, Zhizhpon Eduardo
+ */
+public class CompanyController {
+    
+    private PrivilegeController conPrivilege;
+    private VehCategoryController conCategory;
+
+    public PrivilegeController getConPrivilege() {
+        return conPrivilege;
+    }
+
+    public void setConPrivilege(PrivilegeController conPrivilege) {
+        this.conPrivilege = conPrivilege;
+    }
+
+    public VehCategoryController getConCategory() {
+        return conCategory;
+    }
+
+    public void setConCategory(VehCategoryController conCategory) {
+        this.conCategory = conCategory;
+    }
+    
+}
