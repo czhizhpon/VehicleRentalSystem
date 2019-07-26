acdbe076cf19b4e9e07495e718bb4f1bb8f8b365
Roberto <rserpa@est.ups.edu.ec>
2019-07-25 19:27:32 -0500

    Paquetes: ec.edu.ups.controller y ec.edu.ups.model.
    Desarrollo de controladores ModelController, VehicleController y correción del model Vehicle

--- src/ec/edu/ups/controller/VehicleController.java
+++ src/ec/edu/ups/controller/VehicleController.java
@@ -8,6 +8,7 @@
 package ec.edu.ups.controller;
 
 import ec.edu.ups.conectionDB.ConnectionJava;
+import ec.edu.ups.model.Model;
 import ec.edu.ups.model.VehCategory;
 import ec.edu.ups.model.Vehicle;
 import java.sql.PreparedStatement;
@@ -22,19 +23,25 @@ import java.util.List;
  */
 public class VehicleController {
     
-    private BrandController conBrand;
     private ModelController conModel;
     private VehCategoryController vehCategoryController;
+    private OfficeController conOffice;
     
     private PreparedStatement pstat;
     private ResultSet rstat;
 
-    public BrandController getConBrand() {
-        return conBrand;
+    public VehicleController() {
+        conOffice = new OfficeController();
+        conModel = new ModelController();
+        vehCategoryController = new VehCategoryController();
     }
 
-    public void setConBrand(BrandController conBrand) {
-        this.conBrand = conBrand;
+    public OfficeController getConOffice() {
+        return conOffice;
+    }
+
+    public void setConOffice(OfficeController conOffice) {
+        this.conOffice = conOffice;
     }
 
     public ModelController getConModel() {
@@ -53,17 +60,34 @@ public class VehicleController {
         this.vehCategoryController = vehCategoryController;
     }
     
-    public boolean createVehicle(ConnectionJava connection, Vehicle vehicle){
+    public boolean createVehicle(ConnectionJava connection, Vehicle vehicle,
+            int offId, int modId, int catId, int proId){
         
-        String query = "";
+        String query = "INSERT INTO VRS.VRS_VEHICLES VALUES(\n"
+                + "veh_id_seq.NEXTVAL, ?, ?, ?, ?, ?, "
+                + "?, ?, ?, ?, ?, ?, ?";
         
         try{
             pstat = connection.getConnection().prepareStatement(query);
+            pstat.setString(1, vehicle.getVehPlate());
+            pstat.setString(2, vehicle.getVehColor());
+            pstat.setString(3, "" + vehicle.getVehStatus());
+            pstat.setDouble(4, vehicle.getVehKilometraje());
+            pstat.setString(5, vehicle.getVehInsurranceNumber());
+            pstat.setDate(6, new java.sql.Date(vehicle.getVehInsurranceDate().
+                    getTime()));
+            pstat.setDate(7, new java.sql.Date(vehicle.getVehInsurranceExpires().
+                    getTime()));
+            pstat.setString(8, "" + vehicle.getVehOrigin());
+            pstat.setInt(9, offId);
+            pstat.setInt(10, modId);
+            pstat.setInt(11, catId);
+            pstat.setInt(12, proId);
             
             pstat.executeUpdate();
             
         }catch(SQLException ex){
-            throw new NullPointerException(ex.getSQLState());
+            throw new NullPointerException(ex.toString());
         }
         connection.closeConnection();
         return true;
@@ -73,6 +97,8 @@ public class VehicleController {
             int vehId){
         
         VehCategory vehCategory;
+        Model model;
+        
         String query = "SELECT veh_id, veh_plate, veh_color, veh_status, veh_kilometrage,"
                 + "veh_insurance_number, veh_insurrance_date, veh_insurrance_expires,"
                 + "veh_origin, cat_id"
@@ -98,64 +124,101 @@ public class VehicleController {
                 vehicle.setVehInsurranceExpires(rstat.getDate(8));
                 vehicle.setVehOrigin(rstat.getString(9).charAt(0));
                 
+                model = new Model();
+                
+                this.conModel.readModel(connection, model, rstat.getInt(11));
+                
                 vehCategory = new VehCategory();
                 
                 
                 this.vehCategoryController.readVehCategory(connection, 
-                        vehCategory, rstat.getInt(10));
+                        vehCategory, rstat.getInt(12));
+                
                 vehicle.setVehCategory(vehCategory);
+                
             }
             
         }catch(SQLException ex){
-            throw new NullPointerException(ex.getSQLState());
+            throw new NullPointerException(ex.toString());
         }
         connection.closeConnection();
         return true;
     }
     
     public boolean updateVehicle(ConnectionJava connection, 
-            Vehicle vehicle){
+            Vehicle vehicle, int offId){
         
-        String query = "";
+        String query = "UPDATE vrs.vrs_vehicles SET "
+                + "veh_color = ? "
+                + "veh_status = ? "
+                + "veh_kilometrage = ? "
+                + "veh_insurance_number = ? "
+                + "veh_insurance_date = ? "
+                + "veh_insurance_expires = ? "
+                + "veh_origen = ? "
+                + "off_id = ? "
+                + "mod_id = ? "
+                + "cat_id = ? "
+                + "WHERE veh_id = ?";
         
         try{
+            //pstat = connection.getConnection().prepareStatement(query);
             pstat = connection.getConnection().prepareStatement(query);
+            pstat.setString(1, vehicle.getVehColor());
+            pstat.setString(2, "" + vehicle.getVehStatus());
+            pstat.setDouble(3, vehicle.getVehKilometraje());
+            pstat.setString(4, vehicle.getVehInsurranceNumber());
+            pstat.setDate(5, new java.sql.Date(vehicle.getVehInsurranceDate().
+                    getTime()));
+            pstat.setDate(6, new java.sql.Date(vehicle.getVehInsurranceExpires().
+                    getTime()));
+            pstat.setString(7, "" + vehicle.getVehOrigin());
+            pstat.setInt(8, offId);
+            pstat.setInt(9, vehicle.getVehModel().getModId());
+            pstat.setInt(10, vehicle.getVehCategory().getCatId());
+            
+            pstat.setInt(11, vehicle.getVehId());
             
             pstat.executeUpdate();
             
         }catch(SQLException ex){
-            throw new NullPointerException(ex.getSQLState());
+            throw new NullPointerException(ex.toString());
         }
         connection.closeConnection();
         return true;
     }
     
-    public boolean deleteVehicle(ConnectionJava connection, int vehId){
+    public boolean deleteVehicle(ConnectionJava connection, String vehPlate){
         
-        String query = "";
+        String query = "DELETE vrs.vrs_models "
+                + "WHERE veh_plate = ?";
         
         try{
             pstat = connection.getConnection().prepareStatement(query);
+            pstat.setString(1, vehPlate);
             
             pstat.executeUpdate();
             
         }catch(SQLException ex){
-            throw new NullPointerException(ex.getSQLState());
+            throw new NullPointerException(ex.toString());
         }
         connection.closeConnection();
         return true;
     }
     
     public boolean getVehicles(ConnectionJava connection, 
-            List<Vehicle> vehicles){
+            List<Vehicle> vehicles, Model model){
         
         Vehicle vehicle;
         VehCategory vehCategory;
-        String query = "";
+        String query = "SELECT * "
+                + "FROM vrs.vrs_vehicles "
+                + "WHERE mod_id = ?";
         
         
         try{
             pstat = connection.getConnection().prepareStatement(query);
+            pstat.setInt(1, model.getModId());
             
             rstat = pstat.executeQuery();
             
@@ -164,16 +227,28 @@ public class VehicleController {
                 vehicle = new Vehicle();
                 vehCategory = new VehCategory();
                 
+                vehicle.setVehId(rstat.getInt(1));
+                vehicle.setVehPlate(rstat.getString(2));
+                vehicle.setVehColor(rstat.getString(3));
+                vehicle.setVehStatus(rstat.getString(4).charAt(0));
+                vehicle.setVehKilometraje(rstat.getDouble(5));
+                vehicle.setVehInsurranceNumber(rstat.getString(6));
+                vehicle.setVehInsurranceDate(rstat.getDate(7));
+                vehicle.setVehInsurranceExpires(rstat.getDate(8));
+                vehicle.setVehOrigin(rstat.getString(9).charAt(0));
                 
+                vehicle.setVehModel(model);
                 
                 this.vehCategoryController.readVehCategory(connection, 
-                        vehCategory, 5);
+                        vehCategory, rstat.getInt(12));
+                
                 vehicle.setVehCategory(vehCategory);
+                
                 vehicles.add(vehicle);
             }
             
         }catch(SQLException ex){
-            throw new NullPointerException(ex.getSQLState());
+            throw new NullPointerException(ex.toString());
         }
         connection.closeConnection();
         return true;
